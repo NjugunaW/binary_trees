4d55ce5a871a698ba41408710dcab784428473bf
Chris Mukirae <chriskagiko@gmail.com>
2023-08-31 20:50:35 +0300

    correction 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,6 +6,7 @@
  *@tree: A pointer to the root node of the tree to count the number of nodes.
  * Return: 0 if tree is NULL.
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
@@ -13,4 +14,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->right != NULL || tree->left != NULL)
 		return  (1 + binary_tree_nodes(tree->right) +
 			 binary_tree_nodes(tree->left));
+	return (0);
 }
