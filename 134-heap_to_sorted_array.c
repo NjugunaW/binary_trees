f112e552143c9f621c724d9f3c71b12b74b2d148
NjugunaW <njugunawangui10@gmail.com>
2023-09-01 04:58:55 +0300

    Correction Task 8

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,3 +1,32 @@
+}
+
+/**
+ * heap_to_sorted_array - Creates a sorted array from a max heap
+ * @heap: Pointer to heap's node
+ * @size: Pointer to arrays size
+ *
+ * Return: Pointer to array of integeres
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	int i, nodes, *arr = NULL;
+
+	*size = 0;
+	if (!heap)
+		return (NULL);
+
+	nodes = _count_nodes(heap);
+	arr = malloc(sizeof(*arr) * nodes);
+	if (!arr)
+		return (NULL);
+
+	*size = nodes;
+	for (i = 0; i < nodes; i++)
+		arr[i] = heap_extract(&heap);
+
+	return (arr);
+}
+
 #include "binary_trees.h"
 
 /**
@@ -13,31 +42,4 @@ int _count_nodes(binary_tree_t *root)
 
 	return (1 + _count_nodes(root->left) +
 		    _count_nodes(root->right));
-}
-
-/**
- * heap_to_sorted_array - Creates a sorted array from a max heap
- * @heap: Pointer to heap's node
- * @size: Pointer to arrays size
- *
- * Return: Pointer to array of integeres
- */
-int *heap_to_sorted_array(heap_t *heap, size_t *size)
-{
-	int ax, ndes, *array = NULL;
-
-	*size = 0;
-	if (!heap)
-		return (NULL);
-
-	ndes = _count_nodes(heap);
-	array = malloc(sizeof(*array) * ndes);
-	if (!array)
-		return (NULL);
-
-	*size = ndes;
-	for (ax = 0; ax < ndes; ax++)
-		array[ax] = heap_extract(&heap);
-
-	return (array);
 }
